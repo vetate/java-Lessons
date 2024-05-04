7110d733cbe7a473bdd5152bbed07870517d6b98
Veronica Tate <127885131+vetate@users.noreply.github.com>
2024-05-04 09:27:26 +0100

    Add files via upload

---
+++ SimpleCalc/src/simpleCalc/Driver.java
@@ -0,0 +1,17 @@
+package simpleCalc;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Driver Starts here");
+		
+		SimpleCalc simpleCalc = new SimpleCalc();
+		simpleCalc.add(5, 17);
+		simpleCalc.multiply(33, 55);
+		
+		System.out.println("Driver Ending");
+
+	}//main
+
+}//class
---
+++ SimpleCalc/src/simpleCalc/SimpleCalc.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package simpleCalc;
+
+/**
+ * @author Ve
+ *
+ */
+public class SimpleCalc {
+	
+	//instance vars here
+	
+	// constructors here
+	
+	// methods here
+	public void add(int num1 , int num2) {
+		int answer;
+		answer = num1 + num2;
+		System.out.println("Add is.." + answer);
+	}//Add End
+	
+	public void multiply(int num1 , int num2) {
+		int answer;
+		answer = num1 * num2;
+		System.out.println("Multiply is.." + answer);
+	}//Add End
+	
+	public void subtract(int num1 , int num2) {
+		int answer;
+		answer = num1 - num2;
+		System.out.println("Subtract is.." + answer);
+	}//Add End
+
+}//class
