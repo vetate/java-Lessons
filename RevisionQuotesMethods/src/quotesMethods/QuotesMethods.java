3bc426901e5b35ae6476e4d71f418bfdb2057c29
Veronica Tate <127885131+vetate@users.noreply.github.com>
2024-05-04 09:26:26 +0100

    Add files via upload

---
+++ RevisionQuotesMethods/src/quotesMethods/QuotesMethods.java
@@ -0,0 +1,31 @@
+package quotesMethods;
+
+public class QuotesMethods {
+	
+	public void shakespeareQuote () {
+		System.out.println("Good night, good night! Parting is such sweet sorrow, that I shall say good night.... Shakespeare");
+	}//End ShakespearQuote
+	
+	public void churchillQuote () {
+		System.out.println("Continuious effort - not strength or illegence - is the key to unlocking out potential.... Churchill");
+	}//End ChurchhillQuote
+	
+	public void wildeQuote () {
+		System.out.println("Experience is sinly the name we give our mistakes.... Wilde");
+	}//End wildeQuote
+	
+	public void bushQuote () {
+		System.out.println("One of the great things about boooks is sometimes there are some fantastic pictures.... George W Bush");
+	}//End bushQuote
+	
+public static void main(String args[]) {
+	QuotesMethods quotesMethods = new QuotesMethods();
+	quotesMethods.shakespeareQuote();
+	quotesMethods.wildeQuote();
+	quotesMethods.churchillQuote();
+	quotesMethods.bushQuote();
+	
+	}// main
+	
+}//class
+
